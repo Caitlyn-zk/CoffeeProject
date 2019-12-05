12f185e6cdf83b8e5e01fb286a682ccfd7d25eff
yangjinyao666 <2609529946@qq.com>
2019-12-05 19:15:26 +0800

    杨金耀 12.5 注册页面 按钮封装 购物车按钮点击弹出框

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,30 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
-    <!-- <img src="./assets/logo.png"> -->
-    <router-view/>
-		<!-- <el-button class="padding-10 font-36 line-36" type="">按钮</el-button>
-		<el-button class="nsp-add-button" type="">
-      <i class="el-icon-plus"></i>
-    </el-button> -->
-=======
-<<<<<<< HEAD
-      <merberHome></merberHome>
-=======
-		<Cart></Cart>
->>>>>>> 496c9f7cfdcb7be73a3e86f87b8a556e652037bb
->>>>>>> a4273fdba3c3955a3bc502bc4ec0bfbb5645d400
+    <merberHome></merberHome>
+	<router-view />
+	<Cart></Cart>
   </div>
 </template>
 
 <script>
-<<<<<<< HEAD
-import backTop from './components/Common/backTop/backTop'
-import merberHome from './components/Member/merberHome'
-export default {
-  components: {
-    backTop,
-    merberHome
-=======
 import Nav from 'components/Nav/nav'
 import Navbar from 'components/Nav/navbar'
 import Cart from './components/ShoppingCart'
+import backTop from './components/Common/backTop/backTop'
+import merberHome from './components/Member/merberHome'
 export default {
   name: 'App',
   data () {
-		return {
-			centerDialogVisible: true
+	return {
+		centerDialogVisible: true
 		}
 	},
   components: {
-    Nav,
+    backTop,
+    merberHome,
+	Nav,
     Navbar,
 	Cart
->>>>>>> 496c9f7cfdcb7be73a3e86f87b8a556e652037bb
   }
 }
 </script>
