13e77de83cb41259688d25530296aa7c9dff21b3
LiYongk <2397707341@qq.com>
2019-12-05 18:36:52 +0800

    李永康 第一次提交

---
+++ src/components/Common/backTop/backTop.vue
@@ -0,0 +1,28 @@
+<template>
+  <a class="back-box" @click="backTop">
+      回到顶部<i class="icon-back">^</i>
+  </a>
+</template>
+
+<script>
+export default {
+  methods: {
+    backTop () {
+      let top = document.documentElement.scrollTop || document.body.scrollTop
+        const timeTop = setInterval(() => {
+        document.body.scrollTop -= 50
+        document.documentElement.scrollTop -= 50
+        top -= 50
+        if (top <= 0) {
+          clearInterval(timeTop)
+        }
+      }, 10)
+    }
+  }
+
+}
+</script>
+
+<style lang="less">
+@import './backTopStyle/back.less';
+</style>
