7ff5ffdd0e07f5dad5e1d7dea00c912e00490ecf
X-xucan <2310171316@qq.com>
2019-12-05 20:08:04 +0800

    2019/12/5 徐灿 底部导航栏封装成功

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,20 @@
 <template>
-<<<<<<< HEAD
-  <router-view></router-view>
-=======
   <div id="app">
-<<<<<<< HEAD
     <Nav></Nav>
     <Navbar></Navbar>
-	<router-view />
-=======
+	<router-view></router-view>
     <merberHome></merberHome>
-	<router-view />
 	<Cart></Cart>
->>>>>>> 12063630ece08340d76da5c6eaccef9ce7a83e95
   </div>
->>>>>>> 12f185e6cdf83b8e5e01fb286a682ccfd7d25eff
 </template>
 
 <script>
 import Nav from 'components/Nav/nav'
 import Navbar from 'components/Nav/navbar'
-<<<<<<< HEAD
-export default {
-  name: 'App',
-  components: {
-    Nav,
-    Navbar
-  }
-=======
 import Cart from './components/ShoppingCart'
 import backTop from './components/Common/backTop/backTop'
 import merberHome from './components/Member/merberHome'
 export default {
-<<<<<<< HEAD
-
-=======
   name: 'App',
   data () {
 	return {
@@ -47,15 +28,9 @@ export default {
     Navbar,
 	Cart
   }
->>>>>>> 12f185e6cdf83b8e5e01fb286a682ccfd7d25eff
->>>>>>> 12063630ece08340d76da5c6eaccef9ce7a83e95
 }
 </script>
 
 <style lang="less">
-<<<<<<< HEAD
-@import './public.less';
-=======
 @import './css/public.less';
->>>>>>> 12063630ece08340d76da5c6eaccef9ce7a83e95
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,55 +1,35 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Top from '@/components/Common/top'
-<<<<<<< HEAD
 import Service from '../components/service/Index/Customer'
-=======
-<<<<<<< HEAD
 import Machine from '@/components/Coffeemachine/index'
-=======
 import Order from '@/components/Order/shopping-add.vue'
 import Register from '@/components/Login/register.vue'
->>>>>>> 12f185e6cdf83b8e5e01fb286a682ccfd7d25eff
->>>>>>> 12063630ece08340d76da5c6eaccef9ce7a83e95
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 12063630ece08340d76da5c6eaccef9ce7a83e95
 	{
 		path: '/',
 		name: 'Top',
 		component: Top
     }, {
-<<<<<<< HEAD
 		path: '/service',
 		component: Service
-	}
-=======
+	}, {
+
 		path: '/Machine',
 		name: 'Machine',
 		component: Machine
-=======
-		{
-			path: '/',
-			name: 'Top',
-			component: Top
-    },
-    {
+    }, {
       path: '/order',
       name: 'Order',
       component: Order
-    },
-    {
+    }, {
       path: '/register',
       name: 'Register',
       component: Register
->>>>>>> 12f185e6cdf83b8e5e01fb286a682ccfd7d25eff
     }
->>>>>>> 12063630ece08340d76da5c6eaccef9ce7a83e95
   ]
 })
