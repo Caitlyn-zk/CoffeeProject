729f872f197596633655f469e86431094689f134
guozhitao-theo <2943439858@qq.com>
2019-12-06 19:38:03 +0800

    2019.12.06 郭治涛修改app.vue里面的冲突 （merberHome backtop）

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,16 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
-      <merberHome></merberHome>
-=======
 	<router-view></router-view>
->>>>>>> b45521054d3a6617bdc0baa060b0b3fae865a721
   </div>
 </template>
+
 <script>
-<<<<<<< HEAD
-import backTop from './components/Common/backTop/backTop'
-import merberHome from './components/Member/merberHome'
-export default {
-  components: {
-    backTop,
-    merberHome
-  },
-  data () {
-		return {
-		}
-	}
-=======
-import Nav from 'components/Nav/nav'
-import Navbar from 'components/Nav/navbar'
 import backTop from './components/Common/backTop/backTop'
 export default {
   name: 'App',
-  data () {
-	return {
-		centerDialogVisible: true
-		}
-   },
   components: {
-    backTop,
-    Nav,
-    Navbar
+    backTop
   }
->>>>>>> b45521054d3a6617bdc0baa060b0b3fae865a721
 }
 </script>
 
