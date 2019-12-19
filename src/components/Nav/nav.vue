efe46417a7fd442f33d95de4121ae7f9f30e6c3e
guozhitao-theo <2943439858@qq.com>
2019-12-19 20:01:20 +0800

    2019.12.19 郭治涛 登陆弹窗未请求

--- src/components/Nav/nav.vue
+++ src/components/Nav/nav.vue
@@ -7,6 +7,8 @@
         <div class="nps-user-btn-group fr">
           <button class="nps-user-btn nps-login-account"><i class="nps-login-account-icon"></i>请登陆你的账户 </button>
           <button @click="goodsTable()" class="nps-user-btn nps-shopping-cart"><i class="nps-shopping-cart-icon"></i>您的购物车(0) </button>
+          <!-- 登陆弹窗 -->
+          <Login></Login>
         </div>
       </div>
 			<!-- 侧边购物车 -->
@@ -16,13 +18,15 @@
 
 <script>
 import Cart from '../ShoppingCart/index'
+import Login from 'components/Login/login.vue'
 export default {
 	data () {
 		return {
 		}
 	},
 	components: {
-		Cart
+    Cart,
+    Login
 	},
 	computed: {
 	}
