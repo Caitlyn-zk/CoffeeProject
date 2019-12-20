560f82bfe6fb14c2982429b1957ae812e1de75eb
J-drango-15 <838497293@qq.com>
2019-12-20 14:18:20 +0800

    购物车点击弹出

--- src/components/Nav/nav.vue
+++ src/components/Nav/nav.vue
@@ -4,13 +4,14 @@
         <div class="fl nps-logo-img">
           <img src="./img/xc-logo.png" alt="">
         </div>
+         <div class="fr margin-18">
+          <Cart></Cart>
+        </div>
         <div class="nps-user-btn-group fr">
           <button class="nps-user-btn nps-login-account"><i class="nps-login-account-icon"></i>请登陆你的账户 </button>
-          <button @click="goodsTable()" class="nps-user-btn nps-shopping-cart"><i class="nps-shopping-cart-icon"></i>您的购物车(0) </button>
         </div>
       </div>
 			<!-- 侧边购物车 -->
-			<Cart></Cart>
   </div>
 </template>
 
