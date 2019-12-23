6a0e690bf7b08a98c6e59bb93640539cc17f3deb
Caitlyn-zk <2655816316@qq.com>
2019-12-23 18:26:02 +0800

    2019/12/23 xc 注册界面

--- src/components/Login/register.vue
+++ src/components/Login/register.vue
@@ -1,78 +1,86 @@
 <template>
-  <div class="nps-register-box content">
-    <div class="nps-register-content">
-      <el-form ref="ruleForm" :model="ruleForm" :rules="rules">
-        <el-form-item class="nps-register-form-title">
-          <div class="nps-register-form-item font-12">
-            请填写下方表格。标记为*的为必填字段。
-          </div>
-        </el-form-item>
-        <el-form-item class="nps-register-form-box" prop="surname">
-          <div class="nps-register-form-item">
-            <label class="nps-register-form-label">姓氏
-              <span class="nps-register-asterisk">*</span>
-            </label>
-            <el-tooltip class="item" effect="dark" content="姓氏" placement="right-start">
-              <input v-model="ruleForm.surname" class="nps-register-input" type="text"/>
-            </el-tooltip>
-          </div>
-        </el-form-item>
-        <el-form-item class="nps-register-form-box" prop="name">
-          <div class="nps-register-form-item">
-            <label class="nps-register-form-label">名字
-              <span class="nps-register-asterisk">*</span>
-            </label>
-            <el-tooltip class="item" effect="dark" content="名字" placement="right-start">
-              <input v-model="ruleForm.name" class="nps-register-input" type="text"/>
-            </el-tooltip>
-          </div>
-        </el-form-item>
-        <el-form-item class="nps-register-form-box" prop="email">
-          <div class="nps-register-form-item">
-            <label class="nps-register-form-label">电子邮箱地址
-              <span class="nps-register-asterisk">*</span>
-            </label>
-            <el-tooltip class="item" effect="dark" content="邮箱" placement="right-start">
-              <input v-model="ruleForm.email" class="nps-register-input" type="text"/>
-            </el-tooltip>
-          </div>
-        </el-form-item>
-        <el-form-item class="nps-register-form-box" prop="password">
-           <div class="nps-register-form-item">
-             <label class="nps-register-form-label">密码
-               <span class="nps-register-asterisk">*</span>
-             </label>
-             <el-tooltip class="item" effect="dark" content="密码" placement="right-start">
-              <input v-model="ruleForm.password" class="nps-register-input" type="password"/>
-            </el-tooltip>
-          </div>
-        </el-form-item>
-        <el-form-item class="nps-register-form-remember">
-           <div class="nps-register-form-item">
-             <label class="nps-register-form-label">
-             </label>
-             <input class="nps-register-checkbox" type="checkbox" />
-             <label>记住我</label>
-          </div>
-        </el-form-item>
-      </el-form>
-      <div class="nps-register-bottom">
-        <div class="nps-register-bottom-cont">
-          <span>通过点击 继续”，我同意对我的数据进行处理并同意Nespresso
-            <a class="nps-bottom-cont-declare" href="#">隐私声明</a>
-          </span>
-        </div>
-        <div class="nps-register-bottom-right clearfix">
-          <a class="nps-register-bottom-btn" @click="submitForm('ruleForm')">继续<span class="nps-bottom-crop fr"><i class="el-icon-arrow-right"></i></span></a>
-        </div>
-      </div>
-    </div>
-  </div>
+  <div>
+			<Tips></Tips>
+			<div class="nps-register-back">
+				<div class="nps-register-box content">
+					<div class="nps-register-content">
+						<el-form ref="ruleForm" :model="ruleForm" :rules="rules">
+							<el-form-item class="nps-register-form-title">
+								<div class="nps-register-form-item font-12">
+									请填写下方表格。标记为*的为必填字段。
+								</div>
+							</el-form-item>
+							<el-form-item class="nps-register-form-box" prop="surname">
+								<div class="nps-register-form-item">
+									<label class="nps-register-form-label">姓氏
+										<span class="nps-register-asterisk">*</span>
+									</label>
+									<el-tooltip class="item" effect="dark" content="姓氏" placement="right-start">
+										<input v-model="ruleForm.surname" class="nps-register-input" type="text"/>
+									</el-tooltip>
+								</div>
+							</el-form-item>
+							<el-form-item class="nps-register-form-box" prop="name">
+								<div class="nps-register-form-item">
+									<label class="nps-register-form-label">名字
+										<span class="nps-register-asterisk">*</span>
+									</label>
+									<el-tooltip class="item" effect="dark" content="名字" placement="right-start">
+										<input v-model="ruleForm.name" class="nps-register-input" type="text"/>
+									</el-tooltip>
+								</div>
+							</el-form-item>
+							<el-form-item class="nps-register-form-box" prop="email">
+								<div class="nps-register-form-item">
+									<label class="nps-register-form-label">电子邮箱地址
+										<span class="nps-register-asterisk">*</span>
+									</label>
+									<el-tooltip class="item" effect="dark" content="邮箱" placement="right-start">
+										<input v-model="ruleForm.email" class="nps-register-input" type="text"/>
+									</el-tooltip>
+								</div>
+							</el-form-item>
+							<el-form-item class="nps-register-form-box" prop="password">
+								<div class="nps-register-form-item">
+									<label class="nps-register-form-label">密码
+										<span class="nps-register-asterisk">*</span>
+									</label>
+									<el-tooltip class="item" effect="dark" content="密码" placement="right-start">
+										<input v-model="ruleForm.password" class="nps-register-input" type="password"/>
+									</el-tooltip>
+								</div>
+							</el-form-item>
+							<el-form-item class="nps-register-form-remember">
+								<div class="nps-register-form-item">
+									<label class="nps-register-form-label">
+									</label>
+									<input class="nps-register-checkbox" type="checkbox" />
+									<label>记住我</label>
+								</div>
+							</el-form-item>
+						</el-form>
+						<div class="nps-register-bottom">
+							<div class="nps-register-bottom-cont">
+								<span>通过点击 继续”，我同意对我的数据进行处理并同意Nespresso
+									<a class="nps-bottom-cont-declare" href="#">隐私声明</a>
+								</span>
+							</div>
+							<div class="nps-register-bottom-right clearfix">
+								<a class="nps-register-bottom-btn" @click="submitForm('ruleForm')">继续<span class="nps-bottom-crop fr"><i class="el-icon-arrow-right"></i></span></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<Footer></Footer>
+	</div>
 </template>
 
 <script>
 import {register} from 'commonjs/Requestaxios'
 import {createNamespacedHelpers} from 'vuex'
+import Tips from '../service/Tips'
+import Footer from '../service/Footer'
 const {mapState: registerState, mapMutations: registerMutations} = createNamespacedHelpers('Register')
 export default {
   data () {
@@ -165,8 +173,12 @@ export default {
     }
   },
   computed: {
-    ...registerState(['registerInfo'])
-  }
+		...registerState(['registerInfo'])
+	},
+	components: {
+		Tips,
+		Footer
+	}
 }
 </script>
 
